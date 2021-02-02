2232c7144fc91b83c8094e46b488b65e81be3953
Moritz-Eder-HTL <77103768+Moritz-Eder-HTL@users.noreply.github.com>
2021-02-02 16:20:05 +0100

    2 bilder entfernt

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function prevImg() {
-    let img = document.images[8].src;
+    let img = document.images[6].src;
 
-    for (let i = 8; i > 0; i--) {
+    for (let i = 6; i > 0; i--) {
         document.images[i].src = document.images[i - 1].src;
     }
     document.images[0].src = img;
@@ -10,8 +10,8 @@ function prevImg() {
 function nextImg() {
     let img = document.images[0].src;
 
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < 6; i++) {
         document.images[i].src = document.images[i + 1].src;
     }
-    document.images[8].src = img;
+    document.images[6].src = img;
 }
